1d86494ad2b467d5490ae75485f24e9d2ffac6a2
andrew bolek <andrew.bolek@outlook.com>
2019-04-18 20:58:53 -0400

    Initial commit

---
+++ SportsStore.Domain/Abstract/IOrderProcessor.cs
@@ -0,0 +1,12 @@
+﻿
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.Domain.Abstract
+{
+    //Work being done behind scenes; Cleaner code
+    public interface IOrderProcessor 
+    {
+
+        void ProcessOrder(Cart cart, ShippingDetails shippingDetails);
+    }
+}
---
+++ SportsStore.Domain/Abstract/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SportsStore.Domain.Entities;
+
+//Work being done behind scenes; Cleaner code
+namespace SportsStore.Domain.Abstract
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> Products { get; }
+    }
+}
